326a6504c1322153b7f17f362ff9b305d3b69b60
kimdongzzi <dongzzi2035@gmail.com>
2023-01-06 17:38:02 +0900

    🌱 chore: Create common folder
    by @kimdongzzi

--- screen/Post.js
+++ screen/Post.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { PINK_COLOR } from '../colors';
+import { PINK_COLOR } from '../common/colors';
 
 export default function Post() {
-  return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: PINK_COLOR,
-        width: 200,
-        height: 200,
-        shadowColor: '#000',
-        shadowOffset: { width: 1, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-      }}
-    >
-      <Text>Post</Text>
-    </View>
-  );
+  return <Text>Post</Text>;
 }
