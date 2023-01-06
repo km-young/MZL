8243d95f60c2b8728a79e3da15d7e0edd759ec01
hyoloui <tmdgy9272@gmail.com>
2023-01-06 20:40:40 +0900

    💄 style(Detail.js): create UI
    디테일 페이지 user interface 완성
    
    by @hyoloui

--- screen/Detail.js
+++ screen/Detail.js
@@ -1,6 +1,68 @@
 import React from 'react';
-import { Text } from 'react-native';
+import styled from '@emotion/native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Detail() {
-  return <Text>디테일해요.</Text>;
+  return (
+    <ScrollView>
+      <Section>
+        <Title>단어</Title>
+        <TextBox>
+          <Text>갑분싸</Text>
+        </TextBox>
+      </Section>
+      <Section>
+        <Title>의미</Title>
+
+        <TextBox>
+          <Text>
+            갑자기 분위기 싸해짐 갑자기 분위기를 싸하게 만드는 상황에서 쓰는
+            말이다.
+          </Text>
+        </TextBox>
+      </Section>
+      <Section>
+        <Title>TMI</Title>
+        <TextBox>
+          <Text>
+            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
+            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
+            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
+          </Text>
+        </TextBox>
+      </Section>
+      <ButtonBox onPress={() => {}}>
+        <Btn>
+          <Text>수정</Text>
+        </Btn>
+        <Btn onPress={() => {}}>
+          <Text>삭제</Text>
+        </Btn>
+      </ButtonBox>
+    </ScrollView>
+  );
 }
+
+const Section = styled.View`
+  flex: 1;
+  padding: 40px;
+`;
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+const TextBox = styled.View`
+  flex: 1;
+  padding: 20px;
+  min-height: 70px;
+  background-color: #ffeaa7;
+  box-shadow: 2px 2px 2px #555555;
+`;
+const ButtonBox = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+const Btn = styled.TouchableOpacity`
+  padding: 30px 10px;
+`;
