78cb44656d10dd54ef94256002c91ba74ad4c5b5
hodu_Mom <aldud7798555@gmail.com>
2023-01-07 04:07:32 +0900

    feat(feat/detail): firebase 연결, Edit 컴포넌트 생성, Edit 기능 추가, 이슈 2개 추가

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// 아래 데이터는 본인의 Firebase 프로젝트 설정에서 확인할 수 있습니다.
+const firebaseConfig = {
+  apiKey: 'AIzaSyBTmH7XsYlUMgNCWN93RE7PAHIeiVDTAgI',
+  authDomain: 'rn-words-task.firebaseapp.com',
+  projectId: 'rn-words-task',
+  storageBucket: 'rn-words-task.appspot.com',
+  messagingSenderId: '708302215122',
+  appId: '1:708302215122:web:183931d711d2410c65da0b',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const dbService = getFirestore(app);
+
--- screen/Detail.js
+++ screen/Detail.js
@@ -1,45 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/native';
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {
+  onSnapshot,
+  query,
+  collection,
+  doc,
+  orderBy,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
+import { dbService } from '../firebase';
+import Edit from '../components/Edit';
 
 export default function Detail() {
-  return (
-    <ScrollView>
-      <Section>
-        <Title>단어</Title>
-        <TextBox>
-          <Text>갑분싸</Text>
-        </TextBox>
-      </Section>
-      <Section>
-        <Title>의미</Title>
+  const [category, setCategory] = useState('');
+  const [posts, setPosts] = useState([]);
+  const [mean, setMean] = useState('');
+  const [word, setWord] = useState('');
+  const [tmi, setTmi] = useState('');
 
-        <TextBox>
-          <Text>
-            갑자기 분위기 싸해짐 갑자기 분위기를 싸하게 만드는 상황에서 쓰는
-            말이다.
-          </Text>
-        </TextBox>
-      </Section>
-      <Section>
-        <Title>TMI</Title>
-        <TextBox>
-          <Text>
-            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
-            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
-            배우 황정민이 '갑자기 분뇨를 싸지르다' 라고 답해서 화제가 되었다.
-          </Text>
-        </TextBox>
-      </Section>
-      <ButtonBox onPress={() => {}}>
-        <Btn>
-          <Text>수정</Text>
-        </Btn>
-        <Btn onPress={() => {}}>
-          <Text>삭제</Text>
-        </Btn>
-      </ButtonBox>
-    </ScrollView>
+  const [editMean, SetEditMean] = useState('');
+  const [editWord, SetEditWord] = useState('');
+  const [editTmi, SetEditTmi] = useState('');
+
+  // get해오는부분
+  useEffect(() => {
+    const q = query(
+      collection(dbService, 'Words'),
+      orderBy('createdAt', 'desc'), // 해당 collection 내의 docs들을 createdAt 속성을 내림차순 기준으로
+    );
+
+    onSnapshot(q, (snapshot) => {
+      // q (쿼리)안에 담긴 collection 내의 변화가 생길 때 마다 매번 실행됨
+      const newPosts = snapshot.docs.map((doc) => {
+        const newPost = {
+          id: doc.id,
+          ...doc.data(), // doc.data() : { text, createdAt, ...  }
+        };
+        return newPost;
+      });
+      setPosts(newPosts);
+    });
+
+    const getCategory = async () => {
+      const snapshot = await getDoc(
+        doc(dbService, 'Category', 'currentCategory'),
+      );
+    };
+    getCategory();
+  }, []);
+
+  // 누르면 isEdit이 true/false로 변경됨
+  const setEdit = async (id) => {
+    const idx = posts.findIndex((post) => post.id === id);
+    await updateDoc(doc(dbService, 'Words', id), {
+      isEdit: !posts[idx].isEdit,
+    });
+  };
+
+  // 완료 누르면 글 수정 완료
+  const editPost = async (id) => {
+    await updateDoc(doc(dbService, 'Words', id), {
+      mean: editMean,
+      word: editWord,
+      tmi: editTmi,
+      isEdit: false,
+    });
+  };
+
+  return (
+    <KeyboardAwareScrollView>
+      {/* 이슈1 - if문이나 filter로 id에 해당하는 post만 띄우기, route.params로 해결 가능할 것 같아 강의 복습 예정..-미영- */}
+      {/* 이슈2 - 새로고침 후 수정버튼 누르고 > 아무것도 수정 안하고 등록하면 input이 빈 value로 인식함. 어디서 배웠었는데 기억이안남; -미영- */}
+      {posts.map((post) => {
+        return (
+          <View key={post.id}>
+            {/* isEdit이 true이면 input으로 */}
+            {post.isEdit ? (
+              <Edit
+                SetEditWord={SetEditWord}
+                SetEditTmi={SetEditTmi}
+                SetEditMean={SetEditMean}
+                post={post}
+                editPost={editPost}
+                setEdit={setEdit}
+              />
+            ) : (
+              // isEdit이 false이면 Text로..지영님이 수정할 곳..
+              <>
+                <Section>
+                  <Title>단어</Title>
+                  <TextBox>
+                    <Text>{post.word}</Text>
+                  </TextBox>
+                </Section>
+                <Section>
+                  <Title>의미</Title>
+
+                  <TextBox>
+                    <Text>{post.mean}</Text>
+                  </TextBox>
+                </Section>
+                <Section>
+                  <Title>TMI</Title>
+                  <TextBox>
+                    <Text>{post.tmi}</Text>
+                  </TextBox>
+                </Section>
+                <ButtonBox>
+                  <Btn
+                    onPress={() => {
+                      post.isEdit ? editPost(post.id) : setEdit(post.id);
+                    }}
+                  >
+                    <Text>{post.isEdit ? '등록' : '수정'}</Text>
+                  </Btn>
+                  <Btn onPress={() => {}}>
+                    <Text>{post.isEdit ? '' : '삭제'}</Text>
+                  </Btn>
+                </ButtonBox>
+              </>
+            )}
+          </View>
+        );
+      })}
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -56,8 +146,9 @@ const TextBox = styled.View`
   flex: 1;
   padding: 20px;
   min-height: 70px;
-  background-color: #ffeaa7;
+  background-color: ${(props) => props.background || '#ffeaa7'};
   box-shadow: 2px 2px 2px #555555;
+  overflow: visible;
 `;
 const ButtonBox = styled.View`
   flex-direction: row;
@@ -66,3 +157,4 @@ const ButtonBox = styled.View`
 const Btn = styled.TouchableOpacity`
   padding: 30px 10px;
 `;
+
