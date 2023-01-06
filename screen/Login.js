82513d4ffa995799abbfdef495d268a295051d07
hodu_Mom <aldud7798555@gmail.com>
2023-01-06 19:10:58 +0900

    feat(feat/auth_login): UI Clear

--- screen/Login.js
+++ screen/Login.js
@@ -1,6 +1,64 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
+import styled from '@emotion/native';
 
 export default function Login() {
-  return <Text>로그인해라...</Text>;
+  return (
+    <ContainerView>
+      <StatusBar />
+      <AuthLoginContainerView>
+        <IDInput placeholder="E-MAIL" />
+        <PWInput placeholder="PW" />
+        <Buttons>
+          <ButtonsText>LOGIN</ButtonsText>
+        </Buttons>
+        <Buttons>
+          <ButtonsText>REGISTER</ButtonsText>
+        </Buttons>
+      </AuthLoginContainerView>
+    </ContainerView>
+  );
 }
+
+const ContainerView = styled.View`
+  background-color: white;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const AuthLoginContainerView = styled.View`
+  width: 340px;
+  height: 340px;
+  background-color: #c7f5dd;
+  box-shadow: 1px 4px 4px #808080;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IDInput = styled.TextInput`
+  border-bottom-width: 1px;
+  border-color: #808080;
+  width: 250px;
+  height: 50px;
+`;
+
+const PWInput = styled.TextInput`
+  border-bottom-width: 1px;
+  border-color: #808080;
+  width: 250px;
+  height: 50px;
+`;
+
+const Buttons = styled.TouchableOpacity`
+  height: 20px;
+  width: 100px;
+  text-align: center;
+  margin-top: 30px;
+`
+
+const ButtonsText = styled.Text`
+  text-align: center;
+  height: 20px;
+  line-height: 20px;
+`
